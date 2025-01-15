d1607edcf610cf1c860774d2df5c21d775c5eed0
jgarcia <jgarcia@107-04>
2025-01-15 09:27:02 +0100

    Creado

---
+++ src/main/java/persona/PersonaEntity.java
@@ -0,0 +1,12 @@
+package persona;
+
+public class PersonaEntity {
+
+}
+
+/* Elija la opción que quiera realizar
+ * 1. Guardar Persona
+ * 2. Obtener Persona
+ * 3. Actualizar Persona
+ * 4. Borrar Persona
+ * */
